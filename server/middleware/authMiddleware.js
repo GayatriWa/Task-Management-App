290fe0318e883f363021996ed747612dac5933c0
GayatriWa <gayatridwankhad@gmail.com>
2026-07-17 17:33:49 +0530

    feat: add JWT authentication middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,5 +1,37 @@
-const authMiddleware =  (req, res,next) =>{
+const jwt = require("JsonWebToken")
 
+const authMiddleware =  (req, res,next) =>{
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).json({
+            success:false,
+            message:"Access denied. No token provided"
+        })
+    }
+
+    if(!authHeader.startsWith("Bearer")){
+        return res.status(401).json({
+            success:false,
+            message:"Invalid token Format"
+        })
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = decoded
+
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message:"Invalid or expired token"
+        })
+        
+    }
 }
 
 module.exports = authMiddleware
