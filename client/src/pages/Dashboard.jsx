14c93b66d51fe98642b0340d32c32bff113d6d2e
GayatriWa <gayatridwankhad@gmail.com>
2026-07-18 10:05:37 +0530

    feat: implement logout functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+  
+
+  const handleLogout = () =>{
+    localStorage.removeItem("token");
+    localStorage.removeItem("user")
+    navigate("/login")
+  }
+
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <button onClick={handleLogout}
+      className='bg-red-500 text-white px-4 py-2 rounded' >
+        Logout
+      </button>
+    </div>
   )
 }
 
