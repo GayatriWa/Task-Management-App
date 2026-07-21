04d01dba0ee16eb1fb5f60e4bd338bf64e92840f
GayatriWa <gayatridwankhad@gmail.com>
2026-07-21 22:48:34 +0530

    cors code updated

--- server/app.js
+++ server/app.js
@@ -3,29 +3,38 @@ const cors = require("cors")
 
 const app = express()
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://task-management-app-kohl-mu.vercel.app", // Replace after deploying
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://task-management-app-kohl-mu.vercel.app", // Replace after deploying
+// ];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     credentials: true,
   })
 );
+
+app.use(express.json());
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
 const authRoutes = require("./routes/authRoutes")
 const taskRoutes = require("./routes/taskRoutes")
 
 // Middleware to convert data in json form which come from frontend
-app.use(express.json())
+// app.use(express.json())
 
 // Routes 
 app.use("/api/auth",authRoutes)
