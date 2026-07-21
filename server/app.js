7a51507eade4bbbd2107a4ee0a33b5badf3416c3
GayatriWa <gayatridwankhad@gmail.com>
2026-07-21 23:07:04 +0530

    fix: allow all origins for testing

--- server/app.js
+++ server/app.js
@@ -4,27 +4,35 @@ const cors = require("cors")
 const app = express()
 
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://task-management-app-kohl-mu.vercel.app",
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://task-management-app-kohl-mu.vercel.app",
+// ];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       console.log("Incoming Origin:", origin);
+        // console.log("Incoming Origin:", origin);
+        //     console.log("Allowed:", allowedOrigins);
+
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         console.log("Blocked Origin:", origin);
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      console.log("Incoming Origin:", origin);
-
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.log("Blocked Origin:", origin);
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     credentials: true,
   })
 );
-
 // Middleware to convert data in json form which come from frontend
 app.use(express.json());
 
