45d3c7b2330f8422a4cb3a497dfc01adabc6d2bb
GayatriWa <gayatridwankhad@gmail.com>
2026-07-24 15:06:39 +0530

    useEffect imported

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import { FiX } from "react-icons/fi";
 import { useNavigate } from 'react-router-dom';
 import TaskList from '../components/TaskList';
